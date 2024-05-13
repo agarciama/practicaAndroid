8c0dfefc73d9a410a3f20387fa073701f9557b6f
agarciama <agarciama.inf@upsa.es>
2024-05-13 18:30:55 +0200

    project

---
+++ app/src/main/java/es/upsa/papps/cervantes/Reservas.java
@@ -0,0 +1,38 @@
+package es.upsa.papps.cervantes;
+
+public class Reservas {
+
+
+    private String nombre;
+    private String telefono;
+    private String email;
+
+    public Reservas(String nombre, String telefono, String email) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
